478fa10c8bf736f15f27b015a5c03f58c66a21ec
Raghav-Latawa <raghavlatawa01@gmail.com>
2024-09-24 19:56:30 +0530

    Update map input.py

--- logistic_test_results/map input.py
+++ logistic_test_results/map input.py
@@ -14,20 +14,13 @@ for name in candidates.keys():
 
         data = data.iloc[:, [0, 3]]
         data.columns = ['Sentiment', 'Date']
-        # print(data.head())
-        # data[4] = pd.to_datetime(data[4], format="%m/%d/%Y")
-        # exit(0)
-        dictionary = dict(data['Sentiment'].value_counts())
+       t'].value_counts())
 
         total_len = len(data)
         pos = round((dictionary[4]/total_len) * 100, 2)
         neg = round((dictionary[0]/total_len) * 100, 2)
 
-        # For overall all candidate tweets
-        # row = {'Name': candidates[name], 'POS': dictionary[4], 'NEG': dictionary[0], 'Total': total_len, 'POS%': pos,
-        # 'NEG%': neg}
-
-        # For location wise candidate sentiment
+  
         row = {'Name': candidates[name], 'Location': locations[location], 'POS': dictionary[4], 'NEG': dictionary[0],
                'Total': total_len, 'POS%': pos, 'NEG%': neg}
         print(row)
@@ -35,11 +28,9 @@ for name in candidates.keys():
 
 df = pd.DataFrame(rows)
 print(df)
-# df.to_csv('overall_sentiment for candidates.csv')
-# df.to_csv('locations_sentiment.csv')
+
 print(df.groupby(by=['Location']).max())
 id = df.groupby(by=['Location'])['Total'].idxmax()
 df_max = df.loc[id,]
 print(df_max)
 
-# df_max.to_csv('most_tweeted_candidate_location.csv')
